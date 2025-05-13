610d6e8d0e1cb160fb6df1870782ba7fcbb654eb
gabriel638 <gabriel.medeiros02@souunit.com.br>
2025-05-13 09:10:53 -0300

    'Adição de id de perfis e de unidades'

--- src/config/perfilMap.ts
+++ src/config/perfilMap.ts
@@ -1,4 +1,21 @@
 export const perfilMap: Record<string, number> = {
-  colaborador: 1002392,
-  administrador: 1022032
+  
+  acervodedigilososdaunidade: 100000949,
+  administrador: 100000939,
+  administradordehierarquia: 100000935,
+  administradordesistema: 100000933,
+  administradordetrator: 100000951,
+  administradorsip: 100000931,
+  arquivamento: 100000941,
+  avaliacaodocumental: 100000950,
+  basico: 100000932,
+  basicoduplicado: 100000938, // Nome duplicado com ID diferente
+  cadastrodeusuarioseunidades: 100000937,
+  colaboradorbasicosemassinatura: 100000947,
+  coordenadordeperfil: 100000936,
+  coordenadordeunidade: 100000946,
+  informatica: 100000940,
+  inspecao: 100000945,
+  ouvidoria: 100000944,
+
 };
